ca8609641006f130ab843eb7f5cde4260a1d39e5
Josiah <josiah.pilon@yahoo.ca>
2021-08-02 15:33:30 -0700

    created posts table from running posts migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_02_203223) do
+ActiveRecord::Schema.define(version: 2021_08_02_221957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
