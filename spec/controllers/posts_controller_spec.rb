42e8acb2608f611fee76f2c46686a855228654a0
Josiah <josiah.pilon@yahoo.ca>
2021-08-06 16:31:33 -0700

    added spec to check that a get request to PostsController#new returned a status code of 200

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe PostsController, type: :controller do
+  describe "#new" do
+    it "responds successfully" do
+      get :new
+      expect(response).to be_successful
+    end
+
+    it "returns 200 response" do 
+      get :new
+      expect(response).to have_http_status "200"
+    end
+
+  end
+end
